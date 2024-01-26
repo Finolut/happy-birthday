dcda299b5c1a5c1891f47a2bf6b1b627bb280838
Lutfino Athallah Pratama <finolut@gmail.com>
2024-01-26 23:17:13 +0700

    menambahkan kue

--- script.js
+++ script.js
@@ -16,19 +16,37 @@ function toggleLamp() {
     setTimeout(function () {
         button.style.display = 'none';
 
-        // Periksa apakah body memiliki kelas "lampu-on"
-        if (body.classList.contains("lampu-on")) {
-            // Sembunyikan tombol "Ke acara inti"
-            eventButton.style.display = 'none';
+        var lampImage = document.createElement('img');
+        lampImage.src = 'img/lampu2.png'; // Set the path to your image
 
-            // Tampilkan tombol "Mainkan musik"
-            musicButton.style.display = 'inline-block';
-        } else {
-            // Sembunyikan tombol "Mainkan musik" dan "Ke acara inti"
-            musicButton.style.display = 'none';
-            eventButton.style.display = 'none';
-        }
-    }, 1000);
+        lampImage.style.width = '150%'; // Make it full width
+        lampImage.style.position = 'fixed'; // Position it absolutely
+        lampImage.style.top = 0; // Position it at the top
+        lampImage.style.opacity = 0; // Set initial opacity to 0
+        
+        // Tambahkan elemen img ke body
+        document.body.appendChild(lampImage);
+
+        // Trigger reflow and then add the class to start the fade-in animation
+        lampImage.offsetWidth; // Reflow
+        lampImage.style.opacity = 1; // Set opacity to 1 to start the fade-in effect
+        lampImage.classList.add('fade-out'); // Add a class for further styling if needed
+    }, 100); // Change the delay to adjust the timin
+
+        // Periksa apakah body memiliki kelas "lampu-on"
+        setTimeout(function () {
+            if (body.classList.contains("lampu-on")) {
+                // Sembunyikan tombol "Ke acara inti"
+                eventButton.style.display = 'none';
+    
+                // Tampilkan tombol "Mainkan musik"
+                musicButton.style.display = 'inline-block';
+            } else {
+                // Sembunyikan tombol "Mainkan musik" dan "Ke acara inti"
+                musicButton.style.display = 'none';
+                eventButton.style.display = 'none';
+            }
+        }, 3000);
 }
 
 function playMusic() {
@@ -36,7 +54,7 @@ function playMusic() {
     var eventButton = document.querySelector('.acara');
 
     // Ambil elemen audio
-    audio.src = 'audio/Y2meta.app - Virgoun - Surat Cinta Untuk Starla (tiktok version) (128 kbps).mp3';
+    audio.src = 'audio/Y2meta.app - Saat bahagia sped up + reverb (128 kbps).mp3';
 
     // Mainkan musik
     audio.play();
@@ -48,3 +66,8 @@ function playMusic() {
     eventButton.style.display = 'inline-block';
 }
 
+function startMainEvent() {
+    // Mengubah kelas elemen untuk memunculkan container
+    var containerElement = document.querySelector('.container.center');
+    containerElement.classList.remove('hidden');
+  }
